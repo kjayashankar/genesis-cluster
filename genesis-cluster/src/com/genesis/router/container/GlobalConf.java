30e1ce955bb25576ad4f3c8605789c2fad04918d
nehaparmarcs <np6.023@gmail.com>
2016-11-20 15:40:36 -0800

    Global Conf added

---
+++ genesis-cluster/src/com/genesis/router/container/GlobalConf.java
@@ -0,0 +1,129 @@
+package com.genesis.router.container;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "conf")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class GlobalConf {
+	
+	private String globalPort;
+
+	private String globalHost;
+	
+	private List<RoutingEntry> routing;
+	
+	private String clusterId;
+	
+	
+	public HashMap<String, Integer> asHashMap() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		if (routing != null) {
+			for (RoutingEntry entry : routing) {
+				map.put(entry.host, entry.port);
+			}
+		}
+		return map;
+	}
+
+	public void addEntry(RoutingEntry entry) {
+		if (entry == null)
+			return;
+
+		if (routing == null)
+			routing = new ArrayList<RoutingEntry>();
+
+		routing.add(entry);
+	}
+	
+	
+	public String getGlobalPort ()
+	{
+	    return globalPort;
+	}
+	
+	public void setGlobalPort (String globalPort)
+	{
+	    this.globalPort = globalPort;
+	}
+	
+	public String getGlobalHost ()
+	{
+	    return globalHost;
+	}
+	
+	public void setGlobalHost (String globalHost)
+	{
+	    this.globalHost = globalHost;
+	}
+	
+	public List<RoutingEntry> getRouting ()
+	{
+	    return routing;
+	}
+	
+	public void setRouting (List<RoutingEntry> routing)
+	{
+	    this.routing = routing;
+	}
+	
+	public String getClusterId ()
+	{
+	    return clusterId;
+	}
+	
+	public void setClusterId (String clusterId)
+	{
+	    this.clusterId = clusterId;
+	}
+
+
+
+@XmlRootElement(name = "entry")
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public static final class RoutingEntry {
+	private String host;
+	private int port;
+	private int clusterId;
+
+	public RoutingEntry() {
+	}
+
+	public RoutingEntry(int clusterId, String host, int port) {
+		this.clusterId = clusterId;
+		this.host = host;
+		this.port = port;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public int getClusterId() {
+		return clusterId;
+	}
+
+	public void setClusterId(int clusterId) {
+		this.clusterId = clusterId;
+	}
+
+}
+
+}
